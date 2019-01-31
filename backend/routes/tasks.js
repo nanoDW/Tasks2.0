0de66d4c98a4ba0873ec9f99201b88a8dbb9dea9
nanoDW <s.fluder@interia.eu>
2019-02-01 00:24:45 +0100

    Add routes to handle tasks for other users

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -88,7 +88,7 @@ router.delete("/delete/:id", auth, async (req, res) => {
     const task = await Task.findById(req.params.id);
 
     if (task.user !== req.user._id) {
-      return res.status(403).send("Cannot update task status. Access denied");
+      return res.status(403).send("Cannot delete status. Access denied");
     }
 
     await Task.findByIdAndDelete(req.params._id);
@@ -98,6 +98,77 @@ router.delete("/delete/:id", auth, async (req, res) => {
     console.log(e.message);
 
     return res.status(500).send("Internal server error. Cannot delete task");
+  }
+});
+
+router.post("/user/:id", auth, async (req, res) => {
+  const { body } = req;
+  const { error } = validateTask(body);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  try {
+    const task = new Task({
+      author: req.user.nick,
+      user: req.params.id,
+      description: body.description,
+      priority: body.priority,
+      deadline: body.deadline,
+      created: Date.now(),
+      accepted: false
+    });
+
+    return res.status(200).json({ task });
+  } catch (e) {
+    console.log(e.message);
+
+    return res.status(500).send("Internal server error. Cannot add task");
+  }
+});
+
+router.put("/accept/:id", auth, async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (task.user !== req.user._id) {
+      return res.status(403).send("Access denied. Cannot accept task");
+    }
+
+    task.accepted = true;
+    task.save();
+
+    return res.status(200).json({ task });
+  } catch (e) {
+    console.log(e.message);
+
+    return res.status(500).send("Internal server error. Cannot accept task");
+  }
+});
+
+router.put("/reject/:id", auth, async (req, res) => {
+  if (req.body.note.lenght < 3) {
+    return res
+      .status(400)
+      .send("Cannot make a rejection without substantiation");
+  }
+
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (task.user !== req.user._id) {
+      return res.status(403).send("Access denied. Cannot accept task");
+    }
+
+    task.note = req.body.note;
+    task.save();
+
+    return res.status(200).json({ task });
+  } catch (e) {
+    console.log(e.message);
+
+    return res.status(500).send("Internal server error. Cannot reject task");
   }
 });
 
