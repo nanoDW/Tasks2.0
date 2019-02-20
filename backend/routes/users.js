23505a2c399dbb7be7344033ca0bbf93b9e9758f
nanoDW <s.fluder@interia.eu>
2019-02-20 12:05:10 +0100

    Add account route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcryptjs");
 const config = require("config");
 const jwt = require("jsonwebtoken");
-const { User } = require("../models/schemas");
+const { User, Task, Message } = require("../models/schemas");
 const {
   validateUser,
   validatePasswordAndEmail
@@ -150,6 +150,74 @@ router.get("/me", auth, async (req, res) => {
   const user = await User.findById(req.user._id).select("-password");
   if (!user) return res.status(404).send("User of given id does not exist.");
   return res.json({ user });
+});
+
+router.get("/statistics", auth, async (req, res) => {
+  const { nick, _id } = req.user;
+
+  try {
+    const user = await User.findById(_id).select(
+      "nick last accountCreated friends"
+    );
+    if (!user) return res.status(404).send("User of given id does not exist.");
+
+    const friendsAccepted = user.friends.filter(friend => {
+      return friend.accepted === true;
+    });
+
+    const tasks = await Task.find({
+      $or: [{ user: _id }, { author: nick, accepted: true }]
+    });
+
+    const tasksValues = {
+      active: 0,
+      done: 0,
+      given: 0
+    };
+
+    if (tasks.length > 0) {
+      tasks.map(task => {
+        if (task.user !== _id) tasksValues.given += 1;
+        else if (task.done === false) tasksValues.active += 1;
+        else tasksValues.done += 1;
+        console.log(".");
+      });
+    }
+
+    const messages = await Message.find({
+      $or: [
+        {
+          userID: _id,
+          deletedByUser: false
+        },
+        {
+          authorID: _id,
+          deletedByAuthor: false
+        }
+      ]
+    });
+
+    const sentMessages = messages.filter(message => message.userID === _id);
+    const receivedMessagesLength = messages.length - sentMessages.length;
+
+    const response = {
+      nick: user.nick,
+      createdTasks: user.last,
+      accountCreated: user.accountCreated,
+      friendsAmount: friendsAccepted.length,
+      activeTasks: tasksValues.active,
+      doneTasks: tasksValues.done,
+      givenTasks: tasksValues.given,
+      receivedMessages: receivedMessagesLength,
+      sentMessages: sentMessages.length
+    };
+
+    return res.status(200).json({ response });
+  } catch (e) {
+    console.log(e.message);
+
+    return res.status(500).send("Cannot get data.");
+  }
 });
 
 router.put("/settings", auth, async (req, res) => {
